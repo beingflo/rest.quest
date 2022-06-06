bb494cec70ececa0bf27294764edfb35224ea5ef
Florian Marending <florian@marending.dev>
2022-06-06 20:27:29 +0200

    Add cname

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-  return (
-    <p class="text-2xl text-green-600 text-center py-10">
-      rest.quest new build
-    </p>
-  );
+  return <p class="text-2xl text-green-600 text-center py-10">rest.quest</p>;
 };
 
 export default App;
