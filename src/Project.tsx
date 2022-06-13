90bcc0a216f56b876f6b9ec7079ad42d74f11e16
Florian Marending <florian@marending.dev>
2022-06-13 23:01:30 +0200

    Make edit hover

--- src/Project.tsx
+++ src/Project.tsx
@@ -4,9 +4,11 @@ export type Props = {
 
 const Project = (props: Props) => {
   return (
-    <div class="group">
-      <div>{props.name}</div>
-      <div>Edit</div>
+    <div class="group flex flex-row gap-0.5 items-baseline">
+      <div class="">{props.name}</div>
+      <div class="hidden group-hover:block text-xs text-gray-600 hover:cursor-pointer">
+        Edit
+      </div>
     </div>
   );
 };
