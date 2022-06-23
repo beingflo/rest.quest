e636fc04f3f661cac396ea2ef200bc05afd21c72
Florian Marending <florian@marending.dev>
2022-06-23 21:46:19 +0200

    Allow shortcut input in fields

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 // Execute callback function if event did not target an input
 export const validateEvent = (callback) => (event) => {
-  event.preventDefault();
-  event.target.tagName !== 'INPUT' && callback();
+  if (event.target.tagName !== 'INPUT') {
+    event.preventDefault();
+    callback();
+  }
 };
 
 export const getNewId = () => crypto.randomUUID();
