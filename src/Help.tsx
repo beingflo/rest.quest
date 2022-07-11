c99e89ae4505377f88d5b7bb5153ecfef59479d6
Florian Marending <florian@marending.dev>
2022-07-11 22:06:56 +0200

    Add s3 explanation

--- src/Help.tsx
+++ src/Help.tsx
@@ -17,8 +17,10 @@ const Help: Component = () => {
         <Logo class="w-12 h-12" />
         <h1 class="text-2xl font-bold tracking-tight">rest.quest</h1>
       </div>
-      <p class="mt-4 mb-20">
-        A minimalistic, opinionated todo list application.
+      <p class="mt-4">A minimalistic, opinionated todo list application.</p>
+      <p class="mt-4 mb-10">
+        You're already in the application, press <b>h</b> to toggle the help
+        screen!
       </p>
       <Instruction left="p" right="New project" />
       <Instruction left="n" right="New quest in the current project" />
@@ -28,9 +30,15 @@ const Help: Component = () => {
       <Instruction left="v" right="Toggle view of quests" />
       <Instruction left="arrow up" right="Select previous project" />
       <Instruction left="arrow down" right="Select next project" />
-      <p class="mt-20">
-        You're already in the application, press <b>h</b> to close the help
-        screen!
+      <Instruction left="b y e" right="Purge all local data" />
+      <h2 class="text-xl font-semibold mt-12">S3 synchronization and backup</h2>
+      <p class="mt-4">
+        In the configuration of this app, you can add an endpoint and
+        credentials for an S3 provider. <br />
+      </p>
+      <p class="mt-4 pb-10">
+        If this is provided, the application will store the local state in the
+        S3 bucket when gaining or losing focus.
       </p>
     </div>
   );
