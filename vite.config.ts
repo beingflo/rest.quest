74baec10890b6ef5c2dc273bc18d1c15c78e2320
Florian Marending <florian@marending.dev>
2023-11-11 19:35:56 +0100

    Remote option in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import solidSvg from 'vite-plugin-solid-svg';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
   plugins: [solidPlugin(), solidSvg()],
   build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
+    target: "esnext",
   },
 });
