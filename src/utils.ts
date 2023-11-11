3206b3942cd0954d0006ce824bffbc7b734b8f1c
Florian Marending <florian@marending.dev>
2023-11-11 23:03:03 +0100

    Also ignore textarea

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 // Execute callback function if event did not target an input
 export const validateEvent = (callback) => (event) => {
-  if (event.target.tagName !== 'INPUT') {
+  if (event.target.tagName !== "INPUT" && event.target.tagName !== "TEXTAREA") {
     event.preventDefault();
     callback();
   }
