d125ce469a795eeed41e928ec0d4c09fb0c0ba33
Florian Marending <florian@marending.dev>
2025-05-03 23:26:27 +0200

    [infra] Add deploy script and delete github ci

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+die() { echo "$*" 1>&2 ; exit 1; }
+
+echo -e "Deploying jour.rest.quest to production!"
+
+[ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
+
+npm run eslint || die "ESLint failed"
+npm run build || die "Build failed"
+
+docker --context arm cp dist caddy:/srv/rest.quest || die "Failed to copy files to container"
